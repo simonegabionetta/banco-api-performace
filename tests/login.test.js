56c0c8f3e49a1b1eff42f7d06bc941b8c28bed21
Simone <smgabionetta@yahoo.com.br>
2025-09-25 16:07:32 +0100

    Adicionando a função de helper para obtenção de token e criando o teste de transferencia

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,25 +1,26 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
+const postLogin = JSON.parse(open('../fixtures/postLogin.json'))
 
 export const options = {
+    
     stages: [
     { duration: '5s', target: 10 },
     { duration: '20s', target: 10 },
-    { duration: '5s', target: 0 },
-    
+    { duration: '5s', target: 0 },    
 
   ],
+  
 };
 
 export default function () {
 
     const url = 'http://localhost:3000/login';
 
-    const payload = JSON.stringify({
-        username: 'julio.lima',
-        senha: '123456',
-    });
+    postLogin.username = "junior.lima"    
 
+    const payload = JSON.stringify(postLogin);      
+  
     const params = {
         headers: {
             'Content-Type': 'application/json',
---
+++ tests/transferencias.test.js
@@ -0,0 +1,33 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+import { obterToken } from '../helpers/autenticacao.js';
+
+export const options = {
+  iterations: 1,
+};
+
+export default function () {
+  const token = obterToken();
+  const url = 'http://localhost:3000/transferencias';
+
+  const payload = JSON.stringify({
+    contaOrigem: 1,
+    contaDestino: 2,
+    valor: 10
+  });
+
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    },
+  };
+
+  const res = http.post(url, payload, params);
+
+  check(res, {
+    'status is 201': (r) => r.status === 201,
+  });
+
+  sleep(1);
+}
